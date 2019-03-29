d838a99923d024f323602eb5ca9b9fd5791c5f7c
Robbie  H <ecofemale@hotmail.com>
2019-03-29 23:47:20 +0000

    Automatically backed up by Learn

--- lib/play.rb
+++ lib/play.rb
@@ -37,10 +37,16 @@ end
 
 # Define your play method below
 
-def play(board, index)
-  while play.between 0,8
-    turn(board)
-  end 
-end
+# def play(board, index)
+#   while play.between 0,8
+#     turn(board)
+#   end 
+# end
 
-  
+def play(board)
+   counter = 0
+   until counter == 9
+     turn(board)
+     counter += 1
+   end
+end  
